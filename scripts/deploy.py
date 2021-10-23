7d0baa2be234b3f82cfca8de15f906f14fb47314
spaceh3ad <jan.kwiatkowski@studenci.wsksim.edu.pl>
2021-10-24 01:21:50 +0200

    init repo

---
+++ scripts/deploy.py
@@ -0,0 +1,24 @@
+from brownie import FundMe, network, config
+from scripts.helpful_scripts import get_account
+
+
+def deploy_fund_me():
+    account = get_account()
+
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=True,
+    )
+    print(f"Contract deployed to {fund_me.address}")
+
+
+def main():
+    deploy_fund_me()
