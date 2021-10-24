c0e93e9bd8bc27f7ec605f7f411483e8dc828b89
spaceh3ad <jan.kwiatkowski@studenci.wsksim.edu.pl>
2021-10-24 15:43:46 +0200

    last updates

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,13 +1,17 @@
 from brownie import network, config, accounts, MockV3Aggregator
 from web3 import Web3
 
+FORKED_LOCAL_ENVIROMENTS = ["mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIROMENTS = ["development", "ganache-local"]
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIROMENTS
+    ):
         return accounts[0]
 
     else:
@@ -17,6 +21,4 @@ def get_account():
 def deploy_mocks():
     print(f"Active network is {network.show_active()}")
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(
-            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
-        )
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
